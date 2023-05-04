32e9fe044ac80e9bb2295f2f3b382559ebb33824
oluwasmart <lebioluwasoji@gmail.com>
2023-05-04 01:44:35 -0700

    main y

--- 0x02-minimum_operations/0-main.py
+++ 0x02-minimum_operations/0-main.py
@@ -11,5 +11,3 @@ print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
 n = 12
 print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
 
-n = 9
-print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
